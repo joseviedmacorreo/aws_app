dd883c4121bc149b2c967e461d9feb855758a6c8
Jose Luis Gimenez <joseviedmacorreo@gmail.com>
2023-10-09 08:18:55 -0300

    saco del head

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,9 +8,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
-      {http_equiv: 'Content-Security-Policy', content: 'upgrade-insecure-requests'},
-
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
       ],
     
     link: [
